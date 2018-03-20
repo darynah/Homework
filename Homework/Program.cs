8f0d598988941b4023afce362558612d53f9e025
Darina Gorobets <darina.gorobets@gmail.com>
2018-03-20 21:31:37 +0200

    commit#1

---
+++ Homework/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ship lodka = new Ship(5,"human_engine");
+            lodka.PrintResults();
+
+            Ship kater = new Ship(8, "mechanic");
+            kater.PrintResults();
+
+            Ship plot = new Ship(2);
+            plot.PrintResults();
+
+            Ship nopeople = new Ship("mechanic");
+            nopeople.PrintResults();
+
+
+        }
+    }
+}
---
+++ Homework/Ship.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework
+{
+    public class Ship
+    {
+        public int capacity = 10000;
+        public string engine = "nengine";
+
+        public Ship(int c, string t)
+        {
+            capacity = c*2;
+            engine = t;
+
+        }
+        public Ship(int c)
+        {
+            capacity = c*2;
+
+        }
+        public Ship(string t)
+        {
+            engine = t;
+
+        }
+        public void PrintResults()
+        {
+            Console.WriteLine("capacity: "+ capacity + " engine: "+ engine);
+
+        }
+
+
+
+    }
+}
