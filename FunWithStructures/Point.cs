aabb5f631dfa52564d32c4253aaabbfd6c924dfa
Darina Gorobets <darina.gorobets@gmail.com>
2018-04-08 11:32:49 +0300

    Structure

---
+++ FunWithStructures/Point.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FunWithStructures
+{
+    public struct Point
+    {
+        // поля структуры
+            public int X;
+            public int Y;
+            // добавить 1 к позиции X,Y
+            public void Increment()
+            {
+                X++;
+                Y++;
+            }
+
+            public void Decrement()
+            {
+                X--;
+                Y--;
+            }
+
+            public void Display()
+            {
+                Console.WriteLine("X={0},Y={1}", X, Y);
+            }
+        }
+    }
---
+++ FunWithStructures/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FunWithStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***A First look at structures***\n");
+            Point myPoint;
+            myPoint.X = 249;
+            myPoint.Y = 52;
+            myPoint.Display();
+            // скорректировать значения X,Y
+            myPoint.Increment();
+            myPoint.Display();
+            myPoint.Decrement();
+            myPoint.Display();
+            
+            Point p1 = new Point();
+            p1.X = 10;
+            p1.Y = 20;
+            p1.Display();
+
+            Point p2 = new Point();
+            p2.Display();
+
+            Console.ReadLine();
+        } 
+    }
+}
