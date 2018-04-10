e28180a2f4a4bbc0279871ef5bd99d5acc33d04e
BETLAB\daryna.horobets <daryna.horobets@betlab.com>
2018-04-10 13:47:34 +0300

    Overload

---
+++ Overload/Print.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Overload
+{
+    public class Print
+    {
+        public void Display(string a)
+        {
+            Console.WriteLine();
+        }
+        public void Display(int c)
+        {
+            Console.WriteLine(c);
+        }
+    }
+}
---
+++ Overload/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualBasic;
+
+namespace Overload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Print a = new Print();
+        a.Display(3);
+        a.Display("Hi");
+        Console.ReadLine();
+        }       
+    }
+}
